bca8cc35d234849dee60832e2dae389045506d15
Britt Dawn <brittdawndev@gmail.com>
2017-01-07 14:56:07 -0800

    Get a specific question at api/questions/:questionID. Abstracted question ID.

--- route/question-router.js
+++ route/question-router.js
@@ -9,12 +9,31 @@ const jsonParser = require('body-parser').json();
 const Question = require('../model/question.js');
 const questionRouter = module.exports = Router();
 
+questionRouter.param('questionID', function(req, res, next, questionID) {
+  Question.findById(questionID, function(err, doc) {
+    if(err) return next(err);
+    if(!doc) {
+      err = new Error('Not Found');
+      err.status = 404;
+      return next(err);
+    }
+    req.question = doc;
+    return next();
+  });
+});
 
-// POST /questions - route for creating questions
+// POST api/questions - route for creating questions
 questionRouter.post('/api/questions', jsonParser, function(req, res, next) {
   debug('POST: /api/questions');
 
   new Question(req.body).save()
   .then( question => res.json(question))
   .catch(next);
+});
+
+// // GET api/questions/:questionID - route for a specific questionID
+questionRouter.get('/api/questions/:questionID', function(req, res) {
+  debug('GET: /api/questions/:questionID');
+
+  res.json(req.question);
 });
