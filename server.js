ec89171100c004a4131becc0ee1bcb0769db5f7c
Britt Dawn <brittdawndev@gmail.com>
2017-01-07 14:16:51 -0800

    Adding server file

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-const cors = require('cors'); // eslint-disable-line
-const morgan = require('morgan'); // eslint-disable-line
-const jsonParser = require('body-parser').json;
-const questionRouter = require('./router/question-router.js');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const debug = require('debug')('alexa-skillz:server');
 
-app.use(jsonParser());
+const questionRouter = require('./router/question-router.js');
+const errors = require('./lib/error-middleware.js');
 
-app.use(questionRouter);
+dotenv.load();
+
+mongoose.connect(process.env.MONGODB_URI);
 
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(questionRouter);
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`server is up at port: ${PORT}`);
