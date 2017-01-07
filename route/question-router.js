c498317264703eec8f93912106e92dc5709b3704
Britt Dawn <brittdawndev@gmail.com>
2017-01-07 14:24:38 -0800

    Posting a question at api/questions now works

--- route/question-router.js
+++ route/question-router.js
@@ -1,6 +1,20 @@
 'use strict';
 
-const express = require('express');
-const router = express.Router();
+const Router = require('express').Router;
 
-module.exports = router;
+const debug = require('debug')('alexa-skillz:question-router');
+// const createError = require('http-errors');
+const jsonParser = require('body-parser').json();
+
+const Question = require('../model/question.js');
+const questionRouter = module.exports = Router();
+
+
+// POST /questions - route for creating questions
+questionRouter.post('/api/questions', jsonParser, function(req, res, next) {
+  debug('POST: /api/questions');
+
+  new Question(req.body).save()
+  .then( question => res.json(question))
+  .catch(next);
+});
