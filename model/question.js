93ec0ebd0fe29c59993d2e1c426648a392b5158a
Megan Reardon <meganr@gmail.com>
2017-01-09 11:56:40 -0800

    version showing work on answer array type error in routers

--- model/question.js
+++ model/question.js
@@ -1,27 +1,56 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const answerSchema = require('./model/answer.js');
+// const answerSchema = require('./answer.js');
+
 
 const Schema = mongoose.Schema;
 
-const questionSchema = Schema({
+const AnswerSchema = Schema({
+  content: { type: String, required: true },
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now },
+  votes: { type: Number, default:0 }
+});
+
+const QuestionSchema = Schema({
   content: { type: String, required: true },
   created: { type: Date, required: true, default: Date.now },
   userID: { type: mongoose.Schema.Types.ObjectId, required: true },
-  answers: [answerSchema]
+  answersArray: [AnswerSchema]
 });
 
-const sortAnswers = function(a, b) {
-  if(a.votes === b.votes) {
-    return b.updated = a.updated;
-  }
-  return b.votes - a.votes;
-};
 
-questionSchema.pre('save', function(next) {
-  this.answers.sort(sortAnswers);
-  next();
-});
+// const sortAnswers = function(a, b) {
+//   if(a.votes === b.votes) {
+//     return b.updated = a.updated;
+//   }
+//   return b.votes - a.votes;
+// };
+//
+// questionSchema.pre('save', function(next) {
+//   this.answers.sort(sortAnswers);
+//   next();
+// });
+//
+// answerSchema.method('update', function(updates, callback) {
+//   Object.assign(this, updates, {updated: new Date()});
+//   this.parent().save(callback);
+// });
+//
+// answerSchema.method('vote', function(vote, callback) {
+//   if( vote === 'up') {
+//     this.votes += 1;
+//   } else {
+//     this.votes -= 1;
+//   }
+//   this.parent().save(callback);
+// });
 
-module.exports = mongoose.model('question', questionSchema);
+
+// var Question = module.exports.question = mongoose.model('question', questionSchema);
+// module.exports = mongoose.model('answer', answerSchema);
+
+var Question = mongoose.model('Question', QuestionSchema);
+
+module.exports.Question = Question;
