3bb7e32229af16c5cb6087ba405a653e6c26aeac
Megan Reardon <meganr@gmail.com>
2017-01-09 12:06:59 -0800

    fixes answer array error

--- model/answer.js
+++ model/answer.js
@@ -1,29 +1,26 @@
 'use strict';
 
-// const mongoose = require('mongoose');
-// // const questionSchema = require('./model/question.js');
-//
-// const Schema = mongoose.Schema;
-//
-// const answerSchema = Schema({
-//   content: { type: String, required: true },
-//   created: { type: Date, default: Date.now },
-//   updated: { type: Date, default: Date.now },
-//   votes: { type: Number, default:0 }
-// });
-//
-// answerSchema.method('update', function(updates, callback) {
-//   Object.assign(this, updates, {updated: new Date()});
-//   this.parent().save(callback);
-// });
-//
-// answerSchema.method('vote', function(vote, callback) {
-//   if( vote === 'up') {
-//     this.votes += 1;
-//   } else {
-//     this.votes -= 1;
-//   }
-//   this.parent().save(callback);
-// });
-//
-// module.exports = mongoose.model('answer', answerSchema);
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const answerSchema = Schema({
+  content: { type: String, required: true },
+  created: { type: Date, default: Date.now },
+  updated: { type: Date, default: Date.now },
+  votes: { type: Number, default:0 }
+});
+
+answerSchema.method('update', function(updates, callback) {
+  Object.assign(this, updates, {updated: new Date()});
+  this.parent().save(callback);
+});
+
+answerSchema.method('vote', function(vote, callback) {
+  if( vote === 'up') {
+    this.votes += 1;
+  } else {
+    this.votes -= 1;
+  }
+  this.parent().save(callback);
+});
--- model/question.js
+++ model/question.js
@@ -1,56 +1,27 @@
 'use strict';
 
 const mongoose = require('mongoose');
-// const answerSchema = require('./answer.js');
-
+const answerSchema = require('./answer.js');
 
 const Schema = mongoose.Schema;
 
-const AnswerSchema = Schema({
-  content: { type: String, required: true },
-  created: { type: Date, default: Date.now },
-  updated: { type: Date, default: Date.now },
-  votes: { type: Number, default:0 }
-});
-
-const QuestionSchema = Schema({
+const questionSchema = Schema({
   content: { type: String, required: true },
   created: { type: Date, required: true, default: Date.now },
   userID: { type: mongoose.Schema.Types.ObjectId, required: true },
-  answersArray: [AnswerSchema]
+  answers: [answerSchema]
 });
 
+const sortAnswers = function(a, b) {
+  if(a.votes === b.votes) {
+    return b.updated = a.updated;
+  }
+  return b.votes - a.votes;
+};
 
-// const sortAnswers = function(a, b) {
-//   if(a.votes === b.votes) {
-//     return b.updated = a.updated;
-//   }
-//   return b.votes - a.votes;
-// };
-//
-// questionSchema.pre('save', function(next) {
-//   this.answers.sort(sortAnswers);
-//   next();
-// });
-//
-// answerSchema.method('update', function(updates, callback) {
-//   Object.assign(this, updates, {updated: new Date()});
-//   this.parent().save(callback);
-// });
-//
-// answerSchema.method('vote', function(vote, callback) {
-//   if( vote === 'up') {
-//     this.votes += 1;
-//   } else {
-//     this.votes -= 1;
-//   }
-//   this.parent().save(callback);
-// });
+questionSchema.pre('save', function(next) {
+  this.answers.sort(sortAnswers);
+  next();
+});
 
-
-// var Question = module.exports.question = mongoose.model('question', questionSchema);
-// module.exports = mongoose.model('answer', answerSchema);
-
-var Question = mongoose.model('Question', QuestionSchema);
-
-module.exports.Question = Question;
+module.exports = mongoose.model('question', questionSchema);
