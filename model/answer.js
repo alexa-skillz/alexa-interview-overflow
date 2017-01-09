5d9c4de6fb12bf1eeecef5c8a4da04d7a8ac9b0d
Megan Reardon <meganr@gmail.com>
2017-01-09 10:54:33 -0800

    updated answer and question models to include vote and answer sorting

--- model/answer.js
+++ model/answer.js
@@ -1,14 +1,29 @@
 'use strict';
 
 const mongoose = require('mongoose');
+// const questionSchema = require('./model/question.js');
 
 const Schema = mongoose.Schema;
 
 const answerSchema = Schema({
-  answer: { type: String, required: true },
+  content: { type: String, required: true },
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
-  votes: { type: Number, default:0 },
+  votes: { type: Number, default:0 }
+});
+
+answerSchema.method('update', function(updates, callback) {
+  Object.assign(this, updates, {updated: new Date()});
+  this.parent().save(callback);
+});
+
+answerSchema.method('vote', function(vote, callback) {
+  if( vote === 'up') {
+    this.votes += 1;
+  } else {
+    this.votes -= 1;
+  }
+  this.parent().save(callback);
 });
 
 module.exports = mongoose.model('answer', answerSchema);
--- model/question.js
+++ model/question.js
@@ -1,15 +1,27 @@
 'use strict';
 
 const mongoose = require('mongoose');
-// const answerSchema = require('answer.js');
+const answerSchema = require('./model/answer.js');
 
 const Schema = mongoose.Schema;
 
 const questionSchema = Schema({
   content: { type: String, required: true },
   created: { type: Date, required: true, default: Date.now },
-  userID: { type: mongoose.Schema.Types.ObjectId, required: true }
-  // answers: [answerSchema],
+  userID: { type: mongoose.Schema.Types.ObjectId, required: true },
+  answers: [answerSchema]
+});
+
+const sortAnswers = function(a, b) {
+  if(a.votes === b.votes) {
+    return b.updated = a.updated;
+  }
+  return b.votes - a.votes;
+};
+
+questionSchema.pre('save', function(next) {
+  this.answers.sort(sortAnswers);
+  next();
 });
 
 module.exports = mongoose.model('question', questionSchema);
