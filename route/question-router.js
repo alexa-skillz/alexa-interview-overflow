96be06c0e8db71266a93bce131916ecdb286a679
Britt Dawn <brittdawndev@gmail.com>
2017-02-11 17:32:55 -0800

    Question routes are cleaned up.

--- route/question-router.js
+++ route/question-router.js
@@ -5,6 +5,7 @@ const Router = require('express').Router;
 const debug = require('debug')('alexa-skillz:question-router');
 const User = require('../model/user.js');
 const Question = require('../model/question.js');
+const Answer = require('../model/answer.js');
 const questionRouter = module.exports = Router();
 const jwt = require('express-jwt');
 const auth = jwt({secret: 'secret', userProperty: 'payload'});
@@ -43,82 +44,63 @@ questionRouter.post('/api/questions', auth, jsonParser, function(req, res, next)
     });
   });
 });
-//
-// questionRouter.route("/questions/:question")
-//   .get(function(req, res, next) {
-//     Question.populate(req.question, {
-//       path: "answers",
-//     }).then(function(question) {
-//       Answer.populate(req.question.answers, {
-//         path: "author",
-//         select: "username"
-//       }).then(function(answers) {
-//         res.json(question);
-//       });
-//     });
-//   })
-//   .delete(auth, function(req, res, next) {
-//
-//     // Answer.remove({ question: req.question }, function(err) {
-//     //   if (err) {
-//     //     return next(err);
-//     //   }
-//     //
-//     //   req.question.remove(function(err) {
-//     //     if (err) {
-//     //       return next(err);
-//     //     }
-//     //
-//     //     res.send("success");
-//     //   });
-//     // });
-//   });
-//
-// questionRouter.route("/questions/:question/upvote")
-//   .put(auth, function(req, res, next) {
-//     req.question.upvote(req.payload, function(err, question) {
-//       if (err) {
-//         return next(err);
-//       }
-//
-//       Question.populate(question, {
-//         path: "author",
-//         select: "username"
-//       }).then(function(question) {
-//         res.json(question);
-//       });
-//     });
-//   });
-//
-// questionRouter.route("/questions/:question/downvote")
-//   .put(auth, function(req, res, next) {
-//     req.question.downvote(req.payload, function(err, question) {
-//       if (err) {
-//         return next(err);
-//       }
-//
-//       Question.populate(question, {
-//         path: "author",
-//         select: "username"
-//       }).then(function(question) {
-//         res.json(question);
-//       });
-//     });
-//   });
-//
-// questionRouter.param("question", function(req, res, next, id) {
-//   var query = Question.findById(id);
-//
-//   query.exec(function(err, question) {
-//     if (err) {
-//       return next(err);
-//     }
-//
-//     if (!question) {
-//       return next(new Error("can't find question"));
-//     }
-//
-//     req.question = question;
-//     return next();
-//   });
-// });
+
+questionRouter.get('/api/questions/:question', function(req, res, next){
+  Question.populate(req.question, {
+    path: "answers",
+  }).then(function(question) {
+    Answer.populate(req.question.answers, {
+      path: "author",
+      select: "username"
+    }).then(function(answers) {
+      res.json(question);
+    });
+  });
+})
+
+questionRouter.param('question', function(req, res, next, id){
+  let query = Question.findById(id);
+
+  query.exec(function(err, question) {
+    if (err) {
+      return next(err);
+    }
+
+    if (!question) {
+      return next(new Error('Question not found.'));
+    }
+
+    req.question = question;
+    return next();
+  });
+});
+
+questionRouter.put('/api/questions/:question/upvote', auth, jsonParser, function(req, res, next){
+  req.question.upvote(req.payload, function(err, question) {
+    if (err) {
+      return next(err);
+    }
+
+    Question.populate(question, {
+      path: 'author',
+      select: 'username'
+    }).then(function(question) {
+      res.json(question);
+    });
+  });
+});
+
+questionRouter.put('/api/questions/:question/downvote', auth, jsonParser, function(req, res, next){
+    req.question.downvote(req.payload, function(err, question) {
+      if (err) {
+        return next(err);
+      }
+
+      Question.populate(question, {
+        path: 'author',
+        select: 'username'
+      }).then(function(question) {
+        res.json(question);
+      });
+    });
+  });
